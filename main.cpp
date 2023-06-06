26328b0b06167cbd18cad0eafe95ea20b3b9ff51
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-06-06 18:09:52 +0000

    made int main a one liner

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-
-int main() {
-  std::cout << "Hello World!\n";
+void f(int n)
+{
+  std::cout<<n;
+  if(n>1) f(n-1);
+  if(n>2) f(n-2);
 }
+
+int main()
+{  std::cout << "Hello World!\n"; }
